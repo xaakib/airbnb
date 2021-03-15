35b9207438ecd84c8895ae365236f55c47008f4a
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-15 13:09:15 +0600

    splashscreen done

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -11,15 +11,15 @@ class _SplashScreensState extends State<SplashScreens> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-        seconds: 14,
+        seconds: 5,
         navigateAfterSeconds: MainScreen(),
-        title: new Text(
+        title: Text(
           'Welcome In SplashScreen',
-          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
-        image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        image: Image.asset("assets/images/splashimage.jpg"),
         backgroundColor: Colors.white,
-        styleTextUnderTheLoader: new TextStyle(),
+        styleTextUnderTheLoader: TextStyle(),
         photoSize: 100.0,
         onClick: () => print("Flutter Egypt"),
         loaderColor: Colors.red);
