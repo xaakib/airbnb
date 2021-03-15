5aab791cace7f43e323cc671f57e21f67b2507a1
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-15 13:01:32 +0600

    working on plashScreen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:airbnb/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class SplashScreens extends StatefulWidget {
+  @override
+  _SplashScreensState createState() => _SplashScreensState();
+}
+
+class _SplashScreensState extends State<SplashScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 14,
+        navigateAfterSeconds: MainScreen(),
+        title: new Text(
+          'Welcome In SplashScreen',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.red);
+  }
+}
