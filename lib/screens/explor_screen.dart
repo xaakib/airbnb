8b77d56c6302126efc1830d7160b4d9c8dff4afa
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-15 11:46:34 +0600

    listcard done

--- lib/screens/explor_screen.dart
+++ lib/screens/explor_screen.dart
@@ -5,109 +5,178 @@ class ExplorScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                child: Column(
-                  children: [
-                    Stack(
-                      children: [
-                        Image.network(
-                          "https://a0.muscache.com/pictures/d0370bd5-2344-49c9-a4cf-26229daed288.jpg",
-                          width: MediaQuery.of(context).size.width,
-                          fit: BoxFit.cover,
-                          height: 400,
-                        ),
-                        Positioned(
-                          bottom: 120,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Text(
-                              "Go\nNear",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 70,
-                                color: Colors.white,
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Stack(
+                        children: [
+                          Image.network(
+                            "https://a0.muscache.com/pictures/d0370bd5-2344-49c9-a4cf-26229daed288.jpg",
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                            height: 400,
+                          ),
+                          Positioned(
+                            bottom: 120,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Text(
+                                "Go\nNear",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 70,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 250,
+                          width: MediaQuery.of(context).size.width,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            primary: false,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: 8,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  Container(
+                                      height: 120,
+                                      width: 300,
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                            child: Image.network(
+                                              'https://www.lifewire.com/thmb/BQdMoVsHlk8HaZiZ2tv0elBfFO8=/1920x1080/filters:no_upscale():max_bytes(150000):strip_icc()/tropical-beach-wallpaper-beach-backgrounds-587fbb765f9b584db3241860.jpg',
+                                              height: 100,
+                                              width: 80,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "Rangour",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.black),
+                                              ),
+                                              Text(
+                                                "4.5 hour drive",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    color: Colors.black),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      )),
+                                  Container(
+                                      height: 120,
+                                      width: 300,
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                            child: Image.network(
+                                              'https://www.lifewire.com/thmb/BQdMoVsHlk8HaZiZ2tv0elBfFO8=/1920x1080/filters:no_upscale():max_bytes(150000):strip_icc()/tropical-beach-wallpaper-beach-backgrounds-587fbb765f9b584db3241860.jpg',
+                                              height: 100,
+                                              width: 80,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "Rangour",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.black),
+                                              ),
+                                              Text(
+                                                "4.5 hour drive",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    color: Colors.black),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      )),
+                                ],
+                              );
+                            },
+                          ),
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                        height: 120,
-                        width: 300,
-                        color: Colors.red,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.network(
-                                'https://www.lifewire.com/thmb/BQdMoVsHlk8HaZiZ2tv0elBfFO8=/1920x1080/filters:no_upscale():max_bytes(150000):strip_icc()/tropical-beach-wallpaper-beach-backgrounds-587fbb765f9b584db3241860.jpg',
-                                height: 100,
-                                width: 80,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Rangour",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  "4.5 hour drive",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.normal,
-                                      color: Colors.white),
-                                ),
-                              ],
-                            )
-                          ],
-                        )),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Container(
-                      height: 50,
-                      alignment: Alignment.center,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(30.0)),
-                      child: TextField(
-                        textInputAction: TextInputAction.search,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Search",
-                            hintStyle: TextStyle(color: Colors.black),
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 24.0, vertical: 18.0)),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        height: 50,
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 8.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: TextField(
+                          textInputAction: TextInputAction.search,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search",
+                              hintStyle: TextStyle(color: Colors.black),
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 24.0, vertical: 18.0)),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
